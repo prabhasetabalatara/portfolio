bdcbe6759c73af180a06a172d490ee3a2cdf5f38
prabhasetabalatara <prabhasetabalatara@gmail.com>
2025-11-12 14:36:33 +0700

    Update page.tsx

--- app/[lang]/work/page.tsx
+++ app/[lang]/work/page.tsx
@@ -1,7 +1,7 @@
-import { getAllProjects } from '@/lib/work';
+
+import { getWorkData } from '@/lib/work';
 import WorkView from './_components/WorkView';
 
-// ⬇️ Disamakan dengan layout.tsx
 type WorkPageProps = {
   params: Promise<{
     lang: 'en' | 'id';
@@ -10,25 +10,25 @@ type WorkPageProps = {
 
 export default async function WorkPage({ params }: WorkPageProps) {
   const { lang } = await params;
-  const projects = getAllProjects();
+  const data = getWorkData();
 
   return (
     <div className="container mx-auto px-6 py-24 sm:py-32 lg:px-8">
-      {/* Judul Halaman */}
+      {/* Judul   Halaman */}
       <div className="text-center mb-16">
         <h1 className="text-4xl font-bold tracking-tight text-[var(--foreground)] sm:text-6xl">
-          {lang === 'id' ? 'Projek' : 'Projects'}
+          {data.hero.title[lang]}
         </h1>
         <p className="mt-4 text-lg leading-8 text-[var(--foreground)]/70">
-          {lang === 'id'
-            ? 'Berikut adalah beberapa proyek yang pernah saya kerjakan.'
-            : 'Here are some of the projects I’ve worked on.'}
+          {data.hero.description[lang]}
         </p>
       </div>
 
       {/* Daftar Proyek */}
       <div className="mb-24">
-        <WorkView projects={projects} lang={lang} />
+        <WorkView 
+          projects={data.projects}
+          lang={lang} allLabel={'Semua'} />
       </div>
     </div>
   );
